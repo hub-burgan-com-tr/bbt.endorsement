540f472f3032d703c5db5119a381fc4a97049a52
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-02-08 13:30:47 +0300

    Approval db model added

---
+++ src/Domain/Entities/Approval.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Approval : AuditableEntity, IHasDomainEvent
+    {
+        public string ApprovalId { get; set; } = null!;
+        public string ApprovalTitle { get; set; } = null!;
+        public string InstanceId { get; set; } = null!;
+
+        public List<DomainEvent> DomainEvents { get; set; } = new List<DomainEvent>();
+    }
+}
---
+++ src/Domain/Entities/Reference.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Reference : AuditableEntity
+    {
+        public Reference()
+        {
+            Callbacks = new HashSet<Callback>();
+        }
+
+        [Key]
+        public string ApprovalId { get; set; } = null!;
+        public string InstanceId { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public string Number { get; set; } = null!;
+
+        public virtual Approval Approval { get; set; } = null!;
+        public virtual ICollection<Callback> Callbacks { get; set; }
+    }
+}
