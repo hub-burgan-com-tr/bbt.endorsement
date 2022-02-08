32f6475351a65d41d5f25f5783c51c3992a5ce6a
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-02-08 12:37:11 +0300

    FluentValidation added CreateApprovalCommandValidator added

---
+++ src/Application/Approvals/Commands/CreateApprovalCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Approvals.Commands
+{
+    public class CreateApprovalCommandValidator : AbstractValidator<CreateApprovalCommand>
+    {
+        public CreateApprovalCommandValidator()
+        {
+            RuleFor(v => v.InstanceId).NotEmpty();
+        }
+    }
+}
