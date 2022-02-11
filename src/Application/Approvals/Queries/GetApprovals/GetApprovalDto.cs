bf294d0be8f708b246d6e4aeb1ed12524202149d
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-02-11 11:07:50 +0300

    Mapping and FluentValidation added

--- src/Application/Approvals/Queries/GetApprovals/GetApprovalDto.cs
+++ src/Application/Approvals/Queries/GetApprovals/GetApprovalDto.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Common.Mappings;
+using Domain.Entities;
 
 namespace Application.Approvals.Queries.GetApprovals
 {
-    public class GetApprovalDto
+    public class GetApprovalDto : IMapFrom<Approval>
     {
         public int ApprovalId { get; set; }
         public string ApprovalName { get; set; }
