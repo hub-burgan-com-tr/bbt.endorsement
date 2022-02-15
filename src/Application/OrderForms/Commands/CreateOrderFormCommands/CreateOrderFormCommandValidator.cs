7b88a60181fd1586bae4ca2a880f8c97b4ea3e42
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-02-15 10:00:03 +0300

    merge

--- src/Application/OrderForms/Commands/CreateOrderFormCommands/CreateOrderFormCommandValidator.cs
+++ src/Application/OrderForms/Commands/CreateOrderFormCommands/CreateOrderFormCommandValidator.cs
@@ -6,9 +6,9 @@ namespace Application.OrderForms.Commands.CreateOrderFormCommands
     {
         public CreateOrderFormCommandValidator()
         {
-            RuleFor(v => v.TC).Empty().WithMessage("TCKN girilmelidir.");
+            RuleFor(v => v.CitizenShipNumber).Empty().WithMessage("TCKN girilmelidir.");
 
-            RuleFor(v => v.TC).MaximumLength(11).MinimumLength(11).WithMessage("TCKN 11 haneli olmalıdır.");
+            RuleFor(v => v.CitizenShipNumber).MaximumLength(11).MinimumLength(11).WithMessage("TCKN 11 haneli olmalıdır.");
             RuleFor(v => v.NameAndSurname).Empty().WithMessage("TCKN 11 haneli olmalıdır.");
         }
     }
