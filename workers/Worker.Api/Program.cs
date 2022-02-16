817e9ca2e36563f585ce1c6c6a75449373621439
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-02-16 17:30:19 +0300

    Approval entities name updated

--- workers/Worker.Api/Program.cs
+++ workers/Worker.Api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddInfrastructure(builder);
 
 var app = builder.Build();
 
