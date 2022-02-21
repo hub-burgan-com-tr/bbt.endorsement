a40fa1b19c9c88a9b3b078540a7e44e58d5c9db6
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-02-21 12:57:45 +0300

    merge

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -34,10 +34,10 @@ builder.Services.AddSwaggerGen(options =>
     });
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-    options.CustomSchemaIds(x => x.FullName);
+   options.CustomSchemaIds(x => x.FullName);
     //options.IncludeXmlComments(xmlFilename);
 
-    options.EnableAnnotations(enableAnnotationsForInheritance: true, enableAnnotationsForPolymorphism: true);
+    //options.EnableAnnotations(enableAnnotationsForInheritance: true, enableAnnotationsForPolymorphism: true);
 });
 
 builder.Services.AddApplication();
