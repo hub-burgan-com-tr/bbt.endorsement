8c09003782ba8e389113e3c9915a1075b1c75af0
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-02-21 12:34:27 +0300

    swagger

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddInfrastructure(builder);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.ConfigureSwagger();
     app.UseSwaggerUI(c =>
