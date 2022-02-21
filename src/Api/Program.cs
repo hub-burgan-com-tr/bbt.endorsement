ed2bee793708e956806c8040cb6241c62227f1d2
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-02-21 12:56:10 +0300

    merge startup

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -12,13 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddControllers()
-                .AddJsonOptions(opts =>
-                {
-                    opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                });
-
+    .AddJsonOptions(opts =>
+    {
+        opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 builder.Services.AddSwaggerGen(options =>
 {
@@ -32,18 +30,15 @@ builder.Services.AddSwaggerGen(options =>
             Name = "Contract Approval API",
             Url = new Uri("http://168.119.122.177:9090/my-approval")
         },
-       
+
     });
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-    options.CustomSchemaIds(x => x.FullName);
-    options.IncludeXmlComments(xmlFilename);
+   options.CustomSchemaIds(x => x.FullName);
+    //options.IncludeXmlComments(xmlFilename);
 
     options.EnableAnnotations(enableAnnotationsForInheritance: true, enableAnnotationsForPolymorphism: true);
 });
-
-    
-
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder);
@@ -51,15 +46,18 @@ builder.Services.AddInfrastructure(builder);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.ConfigureSwagger();
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
     app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "bbt.endorsement.api v1");
-                    c.RoutePrefix = "";
-                });
-
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "bbt.endorsement.api v1");
+        c.RoutePrefix = "";
+    });
 }
 
 app.UseHttpsRedirection();
