7de2b6c942dd9ad8645ee4a1f109acd8bd965c50
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-02-24 11:24:12 +0300

    NewOrderCommandTests method add

---
+++ tests/Application.IntegrationTests/Endorsements/Commands/NewOrders/NewOrderCommandTests.cs
@@ -0,0 +1,19 @@
+﻿using Application.Endorsements.Commands.NewOrders;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.Endorsements.Commands.NewOrders;
+using static Testing;
+
+public class NewOrderCommandTests : TestBase
+{
+    [Test]
+    public async Task ExecuteAsync()
+    {
+        var command = new NewOrderCommand();
+        var response = await SendAsync(command);
+        response.Data.InstanceId.Should().BeEmpty();
+    }
+}
+
