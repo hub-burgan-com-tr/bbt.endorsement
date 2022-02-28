6d5b8f738a6e5e3116ef5f1865eef10b0dfe1e74
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-02-28 14:10:18 +0300

    OrderItem added to Domain Layer

--- src/Application/Endorsements/Queries/GetOrders/OrderItem.cs
+++ src/Application/Endorsements/Queries/GetOrders/OrderItem.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Application.Endorsements.Queries.GetOrders;
 
-namespace Application.Endorsements.Queries.GetOrders
+
+public class OrderItem
 {
-    public class OrderItem
+    public Guid Id { get; set; }
+
+    public long Customer { get; set; }
+    public long Approver { get; set; }
+    public ReferenceClass Reference { get; set; }
+
+    public class ReferenceClass
+    {
+        public string Process { get; set; }
+        public string State { get; set; }
+        public Guid Id { get; set; }
+    }
+    public StatusType Status { get; set; }
+    public enum StatusType { Completed, InProgress, Canceled, Halted }
+
+    public DocumentClass[] Documents { get; set; }
+    public class DocumentClass
     {
         public Guid Id { get; set; }
-
-        public long Customer { get; set; }
-        public long Approver { get; set; }
+        public string Name { get; set; }
+        public StatusType Status { get; set; }
+        public enum StatusType { Approved, InProgress, Rejected }
     }
 }
