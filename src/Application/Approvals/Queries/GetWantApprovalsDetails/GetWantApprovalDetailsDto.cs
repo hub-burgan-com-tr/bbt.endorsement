59a64d4c495fb11d006f5473990a316475eb0de1
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-03-07 12:21:01 +0300

    document services  deleted model and dto and controller

--- src/Application/Approvals/Queries/GetWantApprovalsDetails/GetWantApprovalDetailsDto.cs
+++ src/Application/Approvals/Queries/GetWantApprovalsDetails/GetWantApprovalDetailsDto.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Approvals.Commands.CreateApprovalCommands;
-using Application.Documents.Commands.CreateDocumentCommands;
 
 namespace Application.Approvals.Queries.GetWantApprovalsDetails
 {
