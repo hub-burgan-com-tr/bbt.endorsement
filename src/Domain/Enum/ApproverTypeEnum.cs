c5af06f02c0f2b1d7a344cb1e0a4de11b59e68e9
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-03-09 12:14:37 +0300

    Approver order  form services add validator

---
+++ src/Domain/Enum/ApproverTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enum
+{
+    public enum ApproverTypeEnum
+    {
+        CitizenShipNumber=1,
+        CustomerNumber=2
+    }
+}
