9bf98e20e7fec061002a61560ed692fbd0dc8f68
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-03-10 11:02:04 +0300

    StartRequest update

--- src/Application/Endorsements/Commands/NewOrders/StartRequest.cs
+++ src/Application/Endorsements/Commands/NewOrders/StartRequest.cs
@@ -1,69 +1,69 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace Application.Endorsements.Commands.NewOrders;
-
-public class StartRequest
+﻿namespace Application.Endorsements.Commands.NewOrders
 {
-    /// <summary>
-    /// Unique Id of order. Id is corrolation key of workflow also. 
-    /// </summary>
-    public Guid Id { get; set; }
-    public string Title { get; set; }
-    public OrderConfig Config { get; set; }
-    public ReferenceClass Reference { get; set; }
-    public List<DocumentClass> Documents { get; set; }
-    public ApproverClass Approver { get; set; }
-
-    public class DocumentClass
+    public class StartRequest
     {
-        public int DocumentType { get; set; }
+        /// <summary>
+        /// Unique Id of order. Id is corrolation key of workflow also. 
+        /// </summary>
+        public Guid Id { get; set; }
         public string Title { get; set; }
-        public string Content { get; set; }
-        //public Dictionary<string, string> FormParameters { get; set; }
+        public OrderConfig Config { get; set; }
+        public ReferenceClass Reference { get; set; }
+        public List<DocumentClass> Documents { get; set; }
+        public ApproverClass Approver { get; set; }
 
-        //public IFormFile Files { get; set; }
-        public ContentType Type { get; set; }
-        public ActionClass[] Actions { get; set; } // Options
-        public enum ContentType { HTML, PDF, PlainText }
-
-        public class ActionClass
+        public class DocumentClass
         {
-            public bool IsDefault { get; set; }
+            public int DocumentType { get; set; }
             public string Title { get; set; }
-            public ActionType Type { get; set; }
-            public enum ActionType { Approve, Reject }
+            public string Content { get; set; }
+            //public Dictionary<string, string> FormParameters { get; set; }
+
+            //public IFormFile Files { get; set; }
+            public ContentType Type { get; set; }
+            public ActionClass[] Actions { get; set; } // Options
+            public enum ContentType { HTML, PDF, PlainText }
+
+            public class ActionClass
+            {
+                public bool IsDefault { get; set; }
+                public string Title { get; set; }
+                public ActionType Type { get; set; }
+                public enum ActionType { Approve, Reject }
+            }
+        }
+        public class ApproverClass
+        {
+            //public long Customer { get; set; }
+            //public long Approver { get; set; }
+            public int Type { get; set; }
+            public string Value { get; set; }
+            public string NameSurname { get; set; }
+        }
+        public class ReferenceClass
+        {
+            public string Process { get; set; }
+            public string State { get; set; }
+            public string ProcessNo { get; set; }
+            public CallbackClass Callback { get; set; }
+
+        }
+        public class CallbackClass
+        {
+            public CalbackMode Mode { get; set; }
+            public string URL { get; set; }
+            public enum CalbackMode { Completed, Verbose }
+        }
+        public class OrderConfig
+        {
+            public int MaxRetryCount { get; set; }
+            public string RetryFrequence { get; set; }
+            public int ExpireInMinutes { get; set; }
+            public string NotifyMessageSMS { get; set; }
+            public string NotifyMessagePush { get; set; }
+            public string RenotifyMessageSMS { get; set; }
+            public string RenotifyMessagePush { get; set; }
         }
     }
-    public class ApproverClass
-    {
-        //public long Customer { get; set; }
-        //public long Approver { get; set; }
-        public int Type { get; set; }
-        public string Value { get; set; }
-        public string NameSurname { get; set; }
-    }
-    public class ReferenceClass
-    {
-        public string Process { get; set; }
-        public string State { get; set; }
-        public string ProcessNo { get; set; }
-        public CallbackClass Callback { get; set; }
-        
-    }
-    public class CallbackClass
-    {
-        public CalbackMode Mode { get; set; }
-        public string URL { get; set; }
-        public enum CalbackMode { Completed, Verbose }
-    }
-    public class OrderConfig
-    {
-        public int MaxRetryCount { get; set; }
-        public string RetryFrequence { get; set; }
-        public int ExpireInMinutes { get; set; }
-        public string NotifyMessageSMS { get; set; }
-        public string NotifyMessagePush { get; set; }
-        public string RenotifyMessageSMS { get; set; }
-        public string RenotifyMessagePush { get; set; }
-    }
+
 }
