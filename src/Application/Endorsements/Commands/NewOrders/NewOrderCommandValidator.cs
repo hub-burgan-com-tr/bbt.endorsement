83fd9174bbacbf12c694390da1ddc73eb22ff391
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-03-10 10:49:58 +0300

    NewOrderCommandValidator bug fix

--- src/Application/Endorsements/Commands/NewOrders/NewOrderCommandValidator.cs
+++ src/Application/Endorsements/Commands/NewOrders/NewOrderCommandValidator.cs
@@ -17,9 +17,9 @@ namespace Application.Endorsements.Commands.NewOrders
             RuleFor(v => v.Reference.State).Empty().WithMessage("Aşama bilgisi girilmelidir.");
             RuleFor(v => v.Reference.ProcessNo).Empty().WithMessage("İşlem no bilgisi girilmelidir.");
             RuleFor(v => v.Config.RetryFrequence).Empty().WithMessage("Hatırlatma frekansı girilmelidir.");
-            RuleFor(x => x.Documents.Any(x => x.DocumentType == (int)DocumentTypeEnum.Physically)).NotNull().DependentRules(() => {
-                RuleFor(v => v.Documents.Any(x => x.Files == null)).Empty().WithMessage("Lütfen dosya seçiniz");
-            });
+            //RuleFor(x => x.Documents.Any(x => x.DocumentType == (int)DocumentTypeEnum.Physically)).NotNull().DependentRules(() => {
+            //    RuleFor(v => v.Documents.Any(x => x.Files == null)).Empty().WithMessage("Lütfen dosya seçiniz");
+            //});
             RuleFor(x => x.Documents.Any(x => x.DocumentType == (int)DocumentTypeEnum.Text)).NotNull().DependentRules(() => {
                 RuleFor(v => v.Documents.Any(x => x.Content == null)).Empty().WithMessage("Metin girilmelidir.");
                 RuleFor(v => v.Documents.Any(x => x.Title == null)).Empty().WithMessage("Başlık girilmelidir.");
