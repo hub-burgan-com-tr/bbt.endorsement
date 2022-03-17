9cea59f5ef8fb21519b4429973068adddff3b444
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-03-17 11:26:49 +0300

    Get Approval Form detail services add

--- src/Application/Endorsements/Queries/GetApprovalsPhysicallyDocumentDetails/GetApprovalPhysicallyDocumentDetailsDto.cs
+++ src/Application/Endorsements/Queries/GetApprovalsPhysicallyDocumentDetails/GetApprovalPhysicallyDocumentDetailsDto.cs
@@ -1,34 +1,25 @@
 ﻿namespace Application.Endorsements.Queries.GetApprovalsPhysicallyDocumentDetails
 {
     public class GetApprovalPhysicallyDocumentDetailsDto
-    {
-        /// <summary>
-        /// Onay Ad
-        /// </summary>
-        public string ApprovalName { get; set; }
-        /// <summary>
-        /// Onay Başlık
-        /// </summary>
-        public string ApprovalTitle { get; set; }
+    {        
         /// <summary>
         /// Belge Ad
         /// </summary>
-        public string DocumentName { get; set; }
-
+        public string Name { get; set; }
         /// <summary>
         /// Belge Link
         /// </summary>
         public string DocumentLink { get; set; }
 
         /// <summary>
-        /// Onay Aciklamasi
-        /// </summary>
-        public string ApprovalDescription { get; set; }
-
-        /// <summary>
         /// Belge Onaylandı Mı
         /// </summary>
-        public bool IsDocumentApproved { get; set; }
+        public List<Action> Actions { get; set; }
     }
-   
+    public class Action
+    {
+        public bool IsDefault { get; set; }
+        public string Title { get; set; }
+
+    }
 }
--- src/Application/Endorsements/Queries/GetApprovalsPhysicallyDocumentDetails/GetApprovalPhysicallyDocumentDetailsQuery.cs
+++ src/Application/Endorsements/Queries/GetApprovalsPhysicallyDocumentDetails/GetApprovalPhysicallyDocumentDetailsQuery.cs
@@ -1,21 +1,29 @@
-﻿using Application.Common.Models;
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Application.Endorsements.Commands.NewOrders;
+using Domain.Enum;
 using MediatR;
 
 namespace Application.Endorsements.Queries.GetApprovalsPhysicallyDocumentDetails
 {
-    public class GetApprovalPhysicallyDocumentDetailsQuery : IRequest<Response<List<GetApprovalPhysicallyDocumentDetailsDto>>>
+    public class GetApprovalPhysicallyDocumentDetailsQuery : IRequest<Response<GetApprovalPhysicallyDocumentDetailsDto>>
     {
         /// <summary>
         /// Onay Id
         /// </summary>
         public string OrderId { get; set; }
     }
-    public class GetApprovalDocumentDetailsQueryHandler : IRequestHandler<GetApprovalPhysicallyDocumentDetailsQuery, Response<List<GetApprovalPhysicallyDocumentDetailsDto>>>
+    public class GetApprovalDocumentDetailsQueryHandler : IRequestHandler<GetApprovalPhysicallyDocumentDetailsQuery, Response<GetApprovalPhysicallyDocumentDetailsDto>>
     {
-        public async Task<Response<List<GetApprovalPhysicallyDocumentDetailsDto>>> Handle(GetApprovalPhysicallyDocumentDetailsQuery request, CancellationToken cancellationToken)
+        private IApplicationDbContext _context;
+        public GetApprovalDocumentDetailsQueryHandler(IApplicationDbContext context)
         {
-            var list = new List<GetApprovalPhysicallyDocumentDetailsDto>();
-            return Response<List<GetApprovalPhysicallyDocumentDetailsDto>>.Success(list, 200);
+            _context = context;
+        }
+        public async Task<Response<GetApprovalPhysicallyDocumentDetailsDto>> Handle(GetApprovalPhysicallyDocumentDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var response = _context.Documents.Where(x => x.Order.OrderId == request.OrderId && x.Type == DocumentTypeEnum.Physically.ToString()).Select(x => new GetApprovalPhysicallyDocumentDetailsDto { Name = x.Name, Actions = x.Actions.Where(x => x.Type == ContentType.PDF.ToString()).Select(x => new Action { IsDefault = x.IsDefault, Title = x.Title }).ToList() }).FirstOrDefault();
+            return Response<GetApprovalPhysicallyDocumentDetailsDto>.Success(response, 200);
         }
     }
 }
