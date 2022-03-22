6dbea3cf29b44c36966ae6bb5fe09325388fb81a
fatih-ozen-safrantek <fatih.ozen@safrantek.com>
2022-03-22 11:15:35 +0300

    ui changes

--- src/Ui/src/app/screens/my-approval/my-approval.component.ts
+++ src/Ui/src/app/screens/my-approval/my-approval.component.ts
@@ -7,22 +7,37 @@ import {GetEndorsementListRequestModel} from "../../models/my-approval";
   templateUrl: './my-approval.component.html',
   styleUrls: ['./my-approval.component.scss']
 })
+
 export class MyApprovalComponent implements OnInit {
   data: any[];
+
+  pageSize = 10;
+  pageNumber = 1;
+  totalPages = 1;
+  hasNextPage = true;
+  hasPreviousPage = true;
 
   constructor(private myApprovalService: MyApprovalService) {
   }
 
   ngOnInit(): void {
+    this.getEndorsementList();
+  }
+
+  getEndorsementList() {
     let requestModel: GetEndorsementListRequestModel = {
-      pageNumber: 1,
-      pageSize: 10
+      pageNumber: this.pageNumber,
+      pageSize: this.pageSize
     };
     this.myApprovalService.getEndorsementList(requestModel).subscribe({
       next: res => {
-        if (res.data)
+        if (res.data) {
           this.data = res.data.items;
-        else
+          this.pageNumber = res.data.pageNumber;
+          this.totalPages = res.data.totalPages;
+          this.hasNextPage = res.data.hasNextPage;
+          this.hasPreviousPage = res.data.hasPreviousPage;
+        } else
           console.error("Kayıt bulunamadı");
       },
       error: err => {
@@ -31,4 +46,12 @@ export class MyApprovalComponent implements OnInit {
     });
   }
 
+  changePage(changeValue) {
+    this.pageNumber = this.pageNumber + changeValue;
+    this.getEndorsementList();
+  }
+
+  counter(i: number) {
+    return new Array(i);
+  }
 }
