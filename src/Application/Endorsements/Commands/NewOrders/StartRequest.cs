a8661fb6da12e305e191c43547e5e78bc67e5025
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-03-25 12:58:42 +0300

    start request add personel class

--- src/Application/Endorsements/Commands/NewOrders/StartRequest.cs
+++ src/Application/Endorsements/Commands/NewOrders/StartRequest.cs
@@ -62,32 +62,29 @@
     {
         public long ClientNumber { get; set; }
         public long CitizenshipNumber { get; set; }
-
         public NameClass Name { get; set; }
-        public class NameClass
-        {
-            public string First { get; set; }
-            public string Last { get; set; }
-        }
-
         public GsmPhone[] GsmPhones { get; set; }
-
-        public class GsmPhone
-        {
-            public int County { get; set; }
-            public long Prefix { get; set; }
-            public long Number { get; set; }
-        }
-
         public string[] Emails { get; set; }
-
         public Device[] Devices { get; set; }
 
-        public class Device
-        {
-            public string DeviceId { get; set; }
-            public string Label { get; set; }
-        }
+      
     }
+    public class NameClass
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+    }
+    public class Device
+    {
+        public string DeviceId { get; set; }
+        public string Label { get; set; }
+    }
+    public class GsmPhone
+    {
+        public int County { get; set; }
+        public long Prefix { get; set; }
+        public long Number { get; set; }
+    }
+
 
 }
