ed4d5ee81d12fdb581370629b448db3a3bbf3af2
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-03-29 11:47:34 +0300

    Updated OrderApprover

--- workers/Worker.App/Models/OrderApprover.cs
+++ workers/Worker.App/Models/OrderApprover.cs
@@ -6,35 +6,3 @@ public class OrderApprover
     public int Type { get; set; }
     public string Value { get; set; }
 }
-
-    public class OrderApprover_
-{
-    public string NameSurname { get; set; }
-    public int Type { get; set; }
-    public string Value { get; set; }
-
-    public long ClientNumber { get; set; }
-    public long CitizenshipNumber { get; set; }
-    public NameClass Name { get; set; }
-    public GsmPhone[] GsmPhones { get; set; }
-    public string[] Emails { get; set; }
-    public Device[] Devices { get; set; }
-
-    public class NameClass
-    {
-        public string First { get; set; }
-        public string Last { get; set; }
-    }
-    public class Device
-    {
-        public string DeviceId { get; set; }
-        public string Label { get; set; }
-    }
-    public class GsmPhone
-    {
-        public int County { get; set; }
-        public long Prefix { get; set; }
-        public long Number { get; set; }
-    }
-
-}
