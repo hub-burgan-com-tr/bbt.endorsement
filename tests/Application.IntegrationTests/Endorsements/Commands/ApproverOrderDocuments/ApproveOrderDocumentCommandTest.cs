548732ce87e312e9e6a0f93784fe54507b7d5595
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-03-29 17:05:48 +0300

    cancel order and approver order document unit test

---
+++ tests/Application.IntegrationTests/Endorsements/Commands/ApproverOrderDocuments/ApproveOrderDocumentCommandTest.cs
@@ -0,0 +1,23 @@
+﻿using Application.Endorsements.Commands.ApproveOrderDocuments;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.Endorsements.Commands.ApproverOrderDocuments
+{
+    using static Testing;
+    public class ApproveOrderDocumentCommandTest : TestBase
+    {
+        [Test]
+        [TestCase("1beecf76-5529-4309-97fe-39df9e917bd3")]
+        public async Task ApproveOrderDocumentTest(Guid id)
+        {
+            ApproveOrderDocumentCommand model = new ApproveOrderDocumentCommand();
+            var response = await SendAsync(model);
+            Assert.IsNotNull(id);
+        }
+    }
+}
---
+++ tests/Application.IntegrationTests/Endorsements/Commands/CancelOrders/CancelOrderCommandTest.cs
@@ -0,0 +1,23 @@
+﻿using Application.Endorsements.Commands.CancelOrders;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.Endorsements.Commands.CancelOrders
+{
+    using static Testing;
+
+    public class CancelOrderCommandTest:TestBase
+    {
+        [Test]
+        [TestCase("1beecf76-5529-4309-97fe-39df9e917bd3")]
+        public async Task CancelOrderTest(string orderId)
+        {
+            var response = await SendAsync(new CancelOrderCommand { orderId = orderId });
+            Assert.IsNotNull(orderId);
+        }
+    }
+}
