2d9dc3665616a85c4b3209063aaba01ba0f8328f
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-04-01 16:39:41 +0300

    Updated OrderConfig model

--- src/Application/Endorsements/Queries/GetOrderDetails/OrderDetail.cs
+++ src/Application/Endorsements/Queries/GetOrderDetails/OrderDetail.cs
@@ -76,9 +76,9 @@ public class OrderDetail
 
     public class OrderConfig
     {
-        public int MaxRetryCount { get; set; }
+        public int? MaxRetryCount { get; set; }
         public int RetryFrequence { get; set; }
-        public int ExpireInMinutes { get; set; }
+        public int? ExpireInMinutes { get; set; }
         public string NotifyMessageSMS { get; set; }
         public string NotifyMessagePush { get; set; }
         public string RenotifyMessageSMS { get; set; }
