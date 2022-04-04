cc47e58fedf8303327485348f01f046b5edc1e1c
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-04-04 11:55:09 +0300

    Added UpdateDocumentStateCommand

---
+++ workers/Worker.App/Application/Documents/Commands/UpdateDocumentStates/UpdateDocumentStateResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Worker.App.Application.Documents.Commands.UpdateDocumentStates
+{
+    public class UpdateDocumentStateResponse
+    {
+        public bool Completed { get; set; }
+    }
+}
