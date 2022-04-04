8b53532176feeb6fdb7ac9bd89bb7591afb6a5fb
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-04-04 09:47:47 +0300

    Updated CreateOrderHistoryCommand

---
+++ workers/Worker.App/Application/Documents/Commands/CreateDocumentHistories/CreateDocumentHistoryCommand.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Worker.App.Application.Common.Interfaces;
+using Worker.App.Application.Coomon.Models;
+using Worker.App.Domain.Entities;
+
+namespace Worker.AppApplication.Documents.Commands.CreateDocumentHistories;
+
+public class CreateOrderHistoryCommand : IRequest<Response<bool>>
+{
+    public string OrderId { get; set; }
+    public string DocumentId { get; set; }
+    public string State { get; set; }
+    public string Name { get; set; }
+
+}
+
+public class CreateOrderHistoryCommandHandler : IRequestHandler<CreateOrderHistoryCommand, Response<bool>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateOrderHistoryCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Response<bool>> Handle(CreateOrderHistoryCommand request, CancellationToken cancellationToken)
+    {
+        _context.OrderHistories.Add(new OrderHistory
+        {
+            OrderHistoryId = Guid.NewGuid().ToString(),
+            OrderId = request.OrderId,
+            DocumentId = request.DocumentId,
+            State = request.State,
+            Name = request.Name,
+            Created = DateTime.Now
+        });
+        var result = _context.SaveChanges();
+        return Response<bool>.Success(result > 0 ? true : false, 200);
+    }
+}
+
+
+
