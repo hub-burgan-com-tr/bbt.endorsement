6fc34c28d9ab323e6221df326fdf5fb8d0767f0e
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-04-11 10:20:19 +0300

    start request  and form request

--- src/Application/Endorsements/Commands/NewOrderForms/NewOrderFormCommandValidator.cs
+++ src/Application/Endorsements/Commands/NewOrderForms/NewOrderFormCommandValidator.cs
@@ -18,8 +18,8 @@ namespace Application.Endorsements.Commands.NewOrderForms
                 RuleFor(v => v.Reference.Process).NotEmpty().WithMessage("İşlem bilgisi girilmelidir.");
                 RuleFor(v => v.Reference.State).NotEmpty().WithMessage("Aşama bilgisi girilmelidir.");
                 RuleFor(v => v.Reference.ProcessNo).NotEmpty().WithMessage("İşlem no bilgisi girilmelidir.");
-                RuleFor(v => v.Approver.First).NotEmpty().WithMessage("Ad girilmelidir.");
-                RuleFor(v => v.Approver.Last).NotEmpty().WithMessage("Soyad girilmelidir.");
+                RuleFor(v => v.Approver.Name.First).NotEmpty().WithMessage("Ad girilmelidir.");
+                RuleFor(v => v.Approver.Name.Last).NotEmpty().WithMessage("Soyad girilmelidir.");
                 RuleFor(v => v.Approver.CitizenshipNumber.ToString()).MaximumLength(11).MinimumLength(11).WithMessage("TCKN 11 haneli olmalıdır.");
                 
 
