776bec059c745905e37f1d0ad557fa52fd77a634
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-04-12 13:41:45 +0300

    get my approval query

--- src/Application/Endorsements/Queries/GetMyApprovals/GetMyApprovalQuery.cs
+++ src/Application/Endorsements/Queries/GetMyApprovals/GetMyApprovalQuery.cs
@@ -32,7 +32,7 @@ namespace Application.Endorsements.Queries.GetMyApprovals
 
         public async Task<Response<PaginatedList<GetMyApprovalDto>>> Handle(GetMyApprovalQuery request, CancellationToken cancellationToken)
         {
-            var list = await _context.Orders.Where(x => (x.State != OrderState.Pending.ToString()||x.State!=OrderState.Cancel.ToString()))
+            var list = await _context.Orders.Where(x => x.State != OrderState.Pending.ToString()||x.State!=OrderState.Cancel.ToString())
                 .Include(x => x.Documents)
                 .OrderByDescending(x => x.Created)
                 .Select(x => new GetMyApprovalDto
