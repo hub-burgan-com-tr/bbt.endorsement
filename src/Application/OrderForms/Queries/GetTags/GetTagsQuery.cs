323518dce08e97a19037019bffea00008b62bae3
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-04-19 12:54:24 +0300

    Get Form Dto tags

---
+++ src/Application/OrderForms/Queries/GetTags/GetTagsQuery.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.OrderForms.Queries.GetTags
+{
+    public class GetTagsQuery : IRequest<Response<List<GetTagsDto>>>
+    {
+    }
+    public class GetTagsQueryHandler : IRequestHandler<GetTagsQuery, Response<List<GetTagsDto>>>
+    {
+        private IApplicationDbContext _context;
+
+        public GetTagsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Response<List<GetTagsDto>>> Handle(GetTagsQuery request, CancellationToken cancellationToken)
+        {
+            var response = _context.FormDefinitionTags.Select(x => new GetTagsDto { FormDefinitionTagId = x.FormDefinitionTagId, Tag = x.Tag }).OrderBy(x => x.Tag).ToList();
+            return Response<List<GetTagsDto>>.Success(response, 200);
+        }
+    }
+
+
+}
