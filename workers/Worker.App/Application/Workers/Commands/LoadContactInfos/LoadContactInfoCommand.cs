97d410bd09e87b4537e9e74e3af4a685e7ce4d47
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-04-20 14:21:14 +0300

    Updated LoadContactInfoCommand

--- workers/Worker.App/Application/Workers/Commands/LoadContactInfos/LoadContactInfoCommand.cs
+++ workers/Worker.App/Application/Workers/Commands/LoadContactInfos/LoadContactInfoCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Worker.App.Application.Common.Interfaces;
 using Worker.App.Application.Common.Models;
 
@@ -6,21 +7,30 @@ namespace Worker.App.Application.Workers.Commands.LoadContactInfos
 {
     public class LoadContactInfoCommand : IRequest<Response<LoadContactInfoResponse>>
     {
-        public long Id { get; set; }
+        public Guid InstanceId { get; set; }
     }
 
     public class LoadContactInfoCommandHandler : IRequestHandler<LoadContactInfoCommand, Response<LoadContactInfoResponse>>
     {
         private IInternalsService _internalsService;
+        private IApplicationDbContext _context;
 
-        public LoadContactInfoCommandHandler(IInternalsService internalsService)
+        public LoadContactInfoCommandHandler(IInternalsService internalsService, IApplicationDbContext context)
         {
             _internalsService = internalsService;
+            _context = context;
         }
 
         public async Task<Response<LoadContactInfoResponse>> Handle(LoadContactInfoCommand request, CancellationToken cancellationToken)
         {
-            var response = await _internalsService.GetPersonById(request.Id);
+            var order = _context.Orders
+                .Include(x => x.Customer)
+                .Where(x => x.OrderId == request.InstanceId.ToString())
+                .FirstOrDefault();
+            if (order != null)
+                return Response<LoadContactInfoResponse>.NotFoundException("Kayıt bulunamadı",404);
+
+            var response = await _internalsService.GetPersonById(order.Customer.CitizenshipNumber);
             return Response<LoadContactInfoResponse>.Success(new LoadContactInfoResponse { Person = response.Data }, 200);
         }
     }
