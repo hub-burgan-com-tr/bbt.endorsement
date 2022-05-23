59d9242089a68f3a51b7a0b9c6e4773ac97c5ade
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-05-23 19:32:28 +0300

    Updated SaveEntity

--- workers/Worker.App/Application/Workers/Commands/SaveEntities/SaveEntityCommand.cs
+++ workers/Worker.App/Application/Workers/Commands/SaveEntities/SaveEntityCommand.cs
@@ -163,7 +163,7 @@ namespace Worker.App.Application.Workers.Commands.SaveEntities
                 var orderGroup = new OrderGroup { IsCompleted = false, OrderMaps = new List<OrderMap>(), OrderGroupId = Guid.NewGuid().ToString() };
                 if (orderGroup != null)
                 {
-                    orderGroup.OrderMaps.Add(new OrderMap { OrderMapId = Guid.NewGuid().ToString(), OrderGroupId = orderGroup.OrderGroupId, Order = order, DocumentId = document.DocumentId });
+                    orderGroup.OrderMaps.Add(new OrderMap { OrderMapId = Guid.NewGuid().ToString(), OrderGroupId = orderGroup.OrderGroupId, OrderId = order.OrderId, DocumentId = document.DocumentId });
                     var entity = _context.OrderGroups.Add(orderGroup).Entity;
                 }
             }
