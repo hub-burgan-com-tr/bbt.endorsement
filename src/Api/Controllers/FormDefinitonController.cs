a6c600e23aead87c7bf3c55fc12e7c6c9ba31ff5
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-05-26 11:51:58 +0300

    Entity framework update 6..0.5 and dependey reuse add service

--- src/Api/Controllers/FormDefinitonController.cs
+++ src/Api/Controllers/FormDefinitonController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Common.Models;
 using Application.OrderForms.Commands.CreateFormInformations;
+using Application.OrderForms.Commands.UpdateFormDependencyReuse;
 using Application.OrderForms.Commands.UpdateFormInformations;
 using Application.OrderForms.Queries.GetFormInformations;
 using Application.Orders.Queries.GetOrderByFormIds;
@@ -43,6 +44,22 @@ namespace Api.Controllers
             return await Mediator.Send(request);
 
         }
+
+
+        [SwaggerOperation(
+         Summary = "updates form definition dependency reuse",
+         Description = "Form definitons update dependency reuse information"
+     )]
+        [Route("UpdateFormDependencyReuse")]
+        [HttpPost]
+        [SwaggerResponse(200, "Success, form is updated successfully.", typeof(bool))]
+        [SwaggerResponse(201, "Success, form is created successfully.", typeof(bool))]
+        public async Task<Response<bool>> UpdateFormDependencyReuse([FromBody] UpdateFormDependencyReuseCommand request)
+        {
+            return await Mediator.Send(request);
+
+        }
+
 
         [SwaggerOperation(
   Summary = "Get process and form information",
