caa8fb53b209652b7861e169b6599d093af91e72
huseyin-toremen-safrantek <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-05-29 15:27:36 +0300

    Added StaticValuesExtensions

---
+++ workers/Worker.App/Extensions/StaticValuesExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Worker.App.Application.Common.Models;
+using Worker.App.Infrastructure.Configuration.Options;
+
+namespace Worker.App.Extensions;
+
+public static class StaticValuesExtensions
+{
+    public static void SetStaticValues(AppSettings settings)
+    {
+        StaticValues.Sso = settings.Entegration.Sso;
+        StaticValues.Internals = settings.Entegration.Internals;
+        StaticValues.DMSService = settings.Entegration.DMSService;
+        StaticValues.TemplateEngine = settings.Entegration.TemplateEngine;
+    }
+}
+
