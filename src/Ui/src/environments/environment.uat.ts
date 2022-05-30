ad767f94f649c516574a526a24bc720d353befdc
huseyin-toremen-safrantek <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-05-30 11:44:03 +0300

    fix

--- src/Ui/src/environments/environment.uat.ts
+++ src/Ui/src/environments/environment.uat.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl:'https://test-bbt-endorsementapi.apps.nonprod.ebt.bank/'
+  baseUrl:'https://test-bbt-endorsementapi.apps.nonprod.ebt.bank'
 };
