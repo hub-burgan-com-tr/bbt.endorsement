3dc48692143441cb6488abf076bbc69396e2dec1
huseyin-toremen-safrantek <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-06-03 11:26:45 +0300

    Added SendMailTemplateAsync

--- dms/Dms.Integration.Api/Services/MessagingService.cs
+++ dms/Dms.Integration.Api/Services/MessagingService.cs
@@ -7,12 +7,27 @@ namespace Dms.Integration.Api.Services;
 
 public interface IMessagingService
 {
+    Task<SendMailResponse> SendMailTemplateAsync(SendEmailTemplateRequest request);
     Task<MessagingResponse> SendSmsMessageAsync(MessagingRequest request);
     Task<SendMailResponse> SendMailMessageAsync(SendMailRequest request);
 }
 
 public class MessagingService : IMessagingService
 {
+    public Task<SendMailResponse> SendMailTemplateAsync(SendEmailTemplateRequest request)
+    {
+        var url = "https://test-messaginggateway.burgan.com.tr";
+        var restClient = new RestClient(url);
+        var restRequest = new RestRequest("/api/v1/Messaging/email/templated", Method.Post);
+        restRequest.RequestFormat = DataFormat.Json;
+        restRequest.AddJsonBody(request);
+
+        var response = restClient.ExecuteAsync(restRequest).Result;
+        var data = JsonConvert.DeserializeObject<SendMailResponse>(response.Content);
+
+        return Task.FromResult(data);
+    }
+
     public Task<SendMailResponse> SendMailMessageAsync(SendMailRequest request)
     {
         var url = "https://test-messaginggateway.burgan.com.tr";
