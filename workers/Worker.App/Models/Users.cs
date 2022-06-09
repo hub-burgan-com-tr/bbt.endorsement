27f3dab6d8f8a4e4398ea3b1c8dc6c994d6399d4
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-06-09 11:11:31 +0300

    Added Users

---
+++ workers/Worker.App/Models/Users.cs
@@ -0,0 +1,31 @@
+﻿using Worker.App.Application.Internals.Models;
+
+namespace Worker.App.Models;
+
+public static class Users
+{
+    public static List<GsmPhones> GsmPhones()
+    {
+        var list = new List<GsmPhones>
+        {
+            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
+            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
+            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
+            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
+            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
+            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
+            new GsmPhones { County = 90, Prefix = 0, Number = 0 }
+        };
+
+        return list;
+    }
+    public static List<string> Emails()
+    {
+        var list = new List<string>
+        {
+            ""
+        };
+        return list;
+    }
+}
+
