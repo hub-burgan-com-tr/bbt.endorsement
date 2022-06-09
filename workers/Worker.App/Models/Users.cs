628244cff8da1db97d5c5894d83a5f348d8aa172
huseyin-toremen-safrantek <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-06-09 11:19:43 +0300

    Updated Users

--- workers/Worker.App/Models/Users.cs
+++ workers/Worker.App/Models/Users.cs
@@ -8,10 +8,11 @@ public static class Users
     {
         var list = new List<GsmPhones>
         {
-            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
-            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
-            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
-            new GsmPhones { County = 90, Prefix = 0, Number = 0 },
+            new GsmPhones { County = 90, Prefix = 542, Number = 4729390 },
+            new GsmPhones { County = 90, Prefix = 554, Number = 3396048 },
+            new GsmPhones { County = 90, Prefix = 533, Number = 4571673 },
+            new GsmPhones { County = 90, Prefix = 530, Number = 5695988 },
+            new GsmPhones { County = 90, Prefix = 505, Number = 3121304 },
             new GsmPhones { County = 90, Prefix = 0, Number = 0 },
             new GsmPhones { County = 90, Prefix = 0, Number = 0 },
             new GsmPhones { County = 90, Prefix = 0, Number = 0 }
@@ -23,6 +24,13 @@ public static class Users
     {
         var list = new List<string>
         {
+            "htoremen@burgan.com.tr",
+            "KKarakus@burgan.com.tr",
+            "VSerapYilankirkan@burgan.com.tr",
+            "LYaktubay@burgan.com.tr",
+            "FKalender@burgan.com.tr",
+            "",
+            "",
             ""
         };
         return list;
