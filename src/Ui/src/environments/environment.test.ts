8559135010e890d9b8cb6c66c54faacb06332bf4
huseyin-toremen-safrantek <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-06-24 17:40:31 +0300

    Updte loginUrl

--- src/Ui/src/environments/environment.test.ts
+++ src/Ui/src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   baseUrl:'ENDORSEMENTAPI',
-  loginUrl:'https://gondor-identityserver.burgan.com.tr/connect/authorize?response_type=code&client_id=Endorsement&state=EndorsementGondor&scope=roles%20profile&redirect_uri=https://test-bbt-endorsementui.apps.nonprod.ebt.bank/login'
+  loginUrl:'https://gondor-identityserver.burgan.com.tr/connect/authorize?response_type=code&client_id=Endorsement&state=EndorsementGondor&scope=roles&redirect_uri=https://test-bbt-endorsementui.apps.nonprod.ebt.bank/login'
 };
