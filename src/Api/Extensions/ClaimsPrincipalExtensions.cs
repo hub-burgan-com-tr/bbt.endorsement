2a10912c9bb5b99eea528afe973da2e27f641ce8
huseyin-toremen-safrantek <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-07-30 14:06:23 +0300

    Added IsCredentials

--- src/Api/Extensions/ClaimsPrincipalExtensions.cs
+++ src/Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -6,8 +6,13 @@ public static class ClaimsPrincipalExtensions
 {
     public static long GetCitizenshipNumber(this ClaimsPrincipal principal)
     {
-        var citizenshipNumber = long.Parse(principal.Claims.FirstOrDefault(c => c.Type == "username").Value);
+        var citizenshipNumber = long.Parse(principal.Claims.FirstOrDefault(c => c.Type == "username").Value.ToString());
         return citizenshipNumber;
+    }
+    public static bool IsCredentials(this ClaimsPrincipal principal)
+    {
+        var credentials = principal.Claims.Where(c => c.Type == "credentials").ToList();
+        return credentials.Count > 0;
     }
 
     public static string GetUserEmail(this ClaimsPrincipal principal)
