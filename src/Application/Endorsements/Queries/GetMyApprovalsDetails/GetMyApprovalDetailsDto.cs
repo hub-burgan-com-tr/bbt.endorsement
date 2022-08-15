0c201070f7b643e67c345a7f96bd40b8cc14667d
huseyin-toremen-safrantek <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-08-15 14:53:37 +0300

    Updated GetMyApprovalDetailsQuery

--- src/Application/Endorsements/Queries/GetMyApprovalsDetails/GetMyApprovalDetailsDto.cs
+++ src/Application/Endorsements/Queries/GetMyApprovalsDetails/GetMyApprovalDetailsDto.cs
@@ -17,6 +17,7 @@
         public bool State { get; internal set; }
         public string MimeType { get; set; }
         public string OrderState { get; internal set; }
+        public string FileName { get; set; }
     }
     public class Action
     {
