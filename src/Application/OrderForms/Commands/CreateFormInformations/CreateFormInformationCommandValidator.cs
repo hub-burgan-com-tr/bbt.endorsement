e10ebddb43e77917a871ae28f23b565bef59f631
mehmet-cokyasar-safrantek <69903602+mehmet-cokyasar-safrantek@users.noreply.github.com>
2022-08-31 12:17:24 +0300

    form definition update

--- src/Application/OrderForms/Commands/CreateFormInformations/CreateFormInformationCommandValidator.cs
+++ src/Application/OrderForms/Commands/CreateFormInformations/CreateFormInformationCommandValidator.cs
@@ -9,13 +9,19 @@ namespace Application.OrderForms.Commands.CreateFormInformations
 {
     public class CreateFormInformationCommandValidator : AbstractValidator<CreateFormInformationCommand>
     {
+ 
         public CreateFormInformationCommandValidator()
         {
-           // RuleFor(v => v.Name).NotEmpty().WithMessage("Adı girilmelidir.");
-           // RuleFor(v => v.TemplateName).NotEmpty().WithMessage("Template Adı girilmelidir.");
+            RuleFor(v => v.FormDefinitionTagId).NotEmpty().WithMessage("Form Türü  girilmelidir.");
+            RuleFor(v => v.ParameterId).NotEmpty().WithMessage("Parametre Adı girilmelidir.");
+            RuleFor(v => v.Name).NotEmpty().WithMessage("Form Adı girilmelidir.");
+            RuleFor(v => v.TemplateName).NotEmpty().WithMessage("Template Adı girilmelidir.");
             RuleFor(v => v.ExpireInMinutes).NotEmpty().WithMessage("Geçerlilik girilmelidir.");
             RuleFor(v => v.RetryFrequence).NotEmpty().WithMessage("Hatırlatma frekansı girilmelidir.");
             RuleFor(v => v.MaxRetryCount).NotEmpty().WithMessage("Hatırlatma Sayısı girilmelidir.");
+            RuleFor(v => v.Json).NotEmpty().WithMessage("Formio json girilmelidir.");
+            RuleFor(v => v.HtmlTemplate).NotEmpty().WithMessage("Html girilmelidir.");
+
         }
     }
 }
--- src/Application/OrderForms/Commands/UpdateFormInformations/UpdateFormInformationCommandValidator.cs
+++ src/Application/OrderForms/Commands/UpdateFormInformations/UpdateFormInformationCommandValidator.cs
@@ -11,6 +11,10 @@ namespace Application.OrderForms.Commands.UpdateFormInformations
     {
         public UpdateFormInformationCommandValidator()
         {
+            RuleFor(v => v.FormDefinitionId).NotEmpty().WithMessage("Form Id  girilmelidir.");
+            RuleFor(v => v.FormDefinitionTagId).NotEmpty().WithMessage("Form Türü  girilmelidir.");
+            RuleFor(v => v.ParameterId).NotEmpty().WithMessage("Parametre Adı girilmelidir.");
+            RuleFor(v => v.Name).NotEmpty().WithMessage("Form Adı girilmelidir.");
             RuleFor(v => v.TemplateName).NotEmpty().WithMessage("Template Adı girilmelidir.");
             RuleFor(v => v.ExpireInMinutes).NotEmpty().WithMessage("Geçerlilik girilmelidir.");
             RuleFor(v => v.RetryFrequence).NotEmpty().WithMessage("Hatırlatma frekansı girilmelidir.");
