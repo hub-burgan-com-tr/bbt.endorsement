b1dd995c5e07edc5b609d67e1cf6b06f9dad8cf1
fatih <my.fatih.92@gmail.com>
2023-03-27 17:10:41 +0300

    SSO Service Gelistirmeleri

---
+++ src/Application/SSOIntegrationService/Commands/SSOIntegrationResponse.cs
@@ -0,0 +1,17 @@
+﻿using Application.SSOIntegrationService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.SSOIntegrationService.Commands
+{
+    public class SSOIntegrationResponse
+    {
+        public SSOIntegrationResponse() { }
+        public List<UserAuthority> UserAuthorities { get; set; }
+        public UserInfo UserInfo { get; set; }
+        public string RegisterId { get; set; }
+    }
+}
---
+++ tests/Application.IntegrationTests/SSOIntegrationService/SSOIntegrationService.cs
@@ -0,0 +1,30 @@
+﻿using Application.TemplateEngines.Commands.Renders;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.SSOIntegrationService
+{
+    public class SSOIntegrationServiceTest : TestBase
+    {
+
+        [Test]
+        [TestCase("U05804", "","")]
+        public async Task SearchUserInfo(string loginName, string firstName, string lastName)
+        {
+            try
+            {
+                Infrastructure.SSOIntegration.SSOIntegrationService model = new Infrastructure.SSOIntegration.SSOIntegrationService();
+                var data = model.SearchUserInfo("U05804", "", "");
+                Assert.IsNotNull(data);
+            }
+            catch (System.Exception ex)
+            {
+
+            }
+        }
+    }
+}
