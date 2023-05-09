d4c077140d6b2501edc5bbbf011d1a4d5f89b07b
Fatih <my.fatih.92@hotmail.com>
2023-05-09 17:17:41 +0300

    fix

--- src/Ui/src/environments/environment.uat.ts
+++ src/Ui/src/environments/environment.uat.ts
@@ -1,19 +1,10 @@
 export const environment = {
-
   production: false,
-
-  baseUrl: 'https://test-bbt-endorsementapi.apps.nonprod.ebt.bankl',
-
-  loginUrl: 'https://test-bbt-endorsementui.apps.nonprod.ebt.bank/login',
-
-  clientSecret: 'A615C904-4E98-4153-8C53-B9174D4FD32B',
-
-  redirectUri: 'https://test-bbt-endorsementui.apps.nonprod.ebt.bank/login',
-
-  identityServerUrl: 'https://gondor-identityserver.burgan.com.tr',
-
-  clientId: 'EndorsementApi',
-
-  grantType: 'authorization_code'
-
-  };
+  baseUrl: 'ENDORSEMENTAPI',
+  loginUrl: 'LOGINURL',
+  clientSecret: 'CLIENTSECRET',
+  redirectUri: 'REDIRECTURI',
+  identityServerUrl: 'IDENTITYSERVERURL',
+  clientId: 'CLIENTID',
+  grantType: 'GRANTTYPE'
+};
