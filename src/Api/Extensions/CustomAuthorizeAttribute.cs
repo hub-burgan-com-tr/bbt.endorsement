69577a7fd0eb7f3397b103e824a60d14b48ee43f
Fatih <my.fatih.92@hotmail.com>
2024-12-15 21:53:00 +0300

    ok
    ok

--- src/Api/Extensions/CustomAuthorizeAttribute.cs
+++ src/Api/Extensions/CustomAuthorizeAttribute.cs
@@ -1,224 +1,118 @@
 using Infrastructure.SsoServices;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Services;
 using Newtonsoft.Json;
 using Serilog;
-using System.IdentityModel.Tokens.Jwt;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Text;
-[AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
-public class AuthorizeUserAttribute : Attribute, IAuthorizationFilter
-{
-    // public void OnActionExecuted(ActionExecutedContext context)
-    // {
-    //     Log.Information("OnActionExecuted Headers geldi");
-    //     if (context?.HttpContext?.User?.Identity?.IsAuthenticated == true)
-    //     {
-    //         Log.Information("OnActionExecuted Headers IsAuthenticated");
-    //     }
+using System.Threading.Tasks;
 
-    //     Log.Information("OnActionExecuted Headers username" + context?.HttpContext?.User?.Claims?.FirstOrDefault(c => c.Type == "username").Value?.ToString());
-    //     Log.Information("OnActionExecuted Headers FirstOrDefault" + context?.HttpContext?.User?.Claims?.FirstOrDefault().Value?.ToString());
-    //     Log.Information("OnActionExecuted Headers LastOrDefault" + context?.HttpContext?.User?.Claims?.LastOrDefault().Value?.ToString());
-    // }
-    // public void OnActionExecuting(ActionExecutingContext context)
-    // {
-    //     Log.Information("OnActionExecuting Headers:" + JsonConvert.SerializeObject(context.HttpContext?.Request?.Headers));
-    // }
+[AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
+public class AuthorizeUserAttribute : Attribute, IAsyncAuthorizationFilter
+{
     private readonly string _authenticationScheme;
 
     public AuthorizeUserAttribute(string authenticationScheme)
     {
         _authenticationScheme = authenticationScheme;
     }
-    public void OnAuthorization(AuthorizationFilterContext context)
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
     {
-        var log = "OnAuthorizationLogs";
-        Log.Information("OnAuthorization Headers geldi");
-        string access_token = "";
-        string value = context?.HttpContext?.Request?.Headers["Authorization"];
-        if (value.StartsWith("Bearer "))
-        {
-            access_token = value.Substring("Bearer ".Length);
-        }
-        Log.Information(log + "-Bearer ");
-        var userService = context.HttpContext.RequestServices.GetService<IUserService>();
-        Log.Information(log + "-userService ");
+        const string logPrefix = "OnAuthorizationLogs";
+        Log.Information("{LogPrefix} - Authorization started", logPrefix);
 
-        if (userService == null)
+        try
         {
-            Log.Information(log + "userServiceif");
+            // Extract Authorization header
+            string accessToken = "";
+            string authorizationHeader = context?.HttpContext?.Request?.Headers["Authorization"];
+            Log.Information("{LogPrefix} - Authorization header value: {Header}", logPrefix, authorizationHeader);
 
-            context.Result = new UnauthorizedResult();
-            return;
-        }
-        Log.Information(log + "-AccessTokenResource");
-
-        var response = userService.AccessTokenResource(access_token).Result;
-        if (response == null)
-        {
-            Log.Information(log + "-AccessTokenResourceifnull");
-            context.Result = new UnauthorizedResult();
-            return;
-        }
-        Log.Information(log + "-AccessTokenResourceif: {ResponseObject}", JsonConvert.SerializeObject(response));
-        var claims = new List<Claim>
-    {
-        new Claim("CitizenshipNumber", response.CitizenshipNumber ?? ""),
-        new Claim("CustomerNumber", response.CustomerNumber ?? ""),
-        new Claim("FirstName", response.FirstName ?? ""),
-        new Claim("LastName", response.LastName ?? ""),
-        new Claim("IsStaff", response.IsStaff ?? ""),
-        new Claim("BranchCode", response.BranchCode ?? ""),
-        new Claim("BusinessLine", response.BusinessLine ?? ""),
-        new Claim("IsLogin", response.IsLogin.ToString())
-    };
-
-        if (response.Credentials != null)
-        {
-            foreach (var credential in response.Credentials)
+            if (!string.IsNullOrEmpty(authorizationHeader) && authorizationHeader.StartsWith("Bearer "))
             {
-                claims.Add(new Claim("Credential", credential));
+                accessToken = authorizationHeader.Substring("Bearer ".Length);
+                Log.Information("{LogPrefix} - Extracted access token: {Token}", logPrefix, accessToken);
             }
+            else
+            {
+                Log.Warning("{LogPrefix} - Authorization header is invalid or missing", logPrefix);
+            }
+
+            // Resolve UserService
+            var userService = context.HttpContext.RequestServices.GetService<IUserService>();
+            if (userService == null)
+            {
+                Log.Error("{LogPrefix} - UserService could not be resolved", logPrefix);
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            // Fetch user data using access token
+            Log.Information("{LogPrefix} - Fetching user data from AccessTokenResource", logPrefix);
+            var response = await userService.AccessTokenResource(accessToken);
+
+            if (response == null)
+            {
+                Log.Warning("{LogPrefix} - AccessTokenResource returned null for token: {Token}", logPrefix, accessToken);
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            Log.Information("{LogPrefix} - User data fetched successfully: {Response}", logPrefix, JsonConvert.SerializeObject(response));
+
+            // Create claims
+            var claims = new List<Claim>
+            {
+                new Claim("CitizenshipNumber", response.CitizenshipNumber ?? ""),
+                new Claim("CustomerNumber", response.CustomerNumber ?? ""),
+                new Claim("FirstName", response.FirstName ?? ""),
+                new Claim("LastName", response.LastName ?? ""),
+                new Claim("IsStaff", response.IsStaff ?? ""),
+                new Claim("BranchCode", response.BranchCode ?? ""),
+                new Claim("BusinessLine", response.BusinessLine ?? ""),
+                new Claim("IsLogin", response.IsLogin.ToString())
+            };
+
+            Log.Information("{LogPrefix} - Core claims added: {Claims}", logPrefix, claims.Select(c => c.Type));
+
+            // Add credentials to claims
+            if (response.Credentials != null && response.Credentials.Any())
+            {
+                foreach (var credential in response.Credentials)
+                {
+                    claims.Add(new Claim("Credential", credential));
+                }
+
+                Log.Information("{LogPrefix} - Credentials added to claims: {Credentials}", logPrefix, response.Credentials);
+            }
+
+            // Create identity and principal
+            var identity = new ClaimsIdentity(claims, _authenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+            context.HttpContext.User = principal;
+
+            Log.Information("{LogPrefix} - Claims successfully added to HttpContext", logPrefix);
+
+            // Validate user presence
+            if (context.HttpContext.User == null)
+            {
+                Log.Error("{LogPrefix} - User is null after setting claims", logPrefix);
+            }
+            else
+            {
+                var userClaims = context.HttpContext.User.Claims.Select(c => new { c.Type, c.Value }).ToList();
+                Log.Information("{LogPrefix} - User claims: {Claims}", logPrefix, JsonConvert.SerializeObject(userClaims));
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "{LogPrefix} - Exception occurred during authorization", logPrefix);
+            context.Result = new UnauthorizedResult();
         }
 
-        var identity = new ClaimsIdentity(claims, _authenticationScheme);
-        var principal = new ClaimsPrincipal(identity);
-        context.HttpContext.User = principal;
-
-        if (context.HttpContext.User is null)
-        {
-            Log.Information(log + "-User null");
-        }
-        else
-        {
-            Log.Information("OnAuthorization Headers username" + context?.HttpContext?.User?.Claims?.FirstOrDefault(c => c.Type == "username").Value?.ToString());
-            Log.Information("OnAuthorization Headers FirstOrDefault" + context?.HttpContext?.User?.Claims?.FirstOrDefault().Value?.ToString());
-            Log.Information("OnAuthorization Headers LastOrDefault" + context?.HttpContext?.User?.Claims?.LastOrDefault().Value?.ToString());
-        }
-
-        Log.Information(log + "-end");
-        return;
-
-        // if (!string.IsNullOrEmpty(response.Token))
-        // {
-        //     try
-        //     {
-        //         // Token'ı doğrula ve kullanıcıya ata
-        //         var principal = ValidateToken(token);
-        //         if (principal != null)
-        //         {
-        //             context.HttpContext.User = principal;
-        //         }
-        //         else
-        //         {
-        //             context.Result = new UnauthorizedResult();
-        //         }
-        //     }
-        //     catch
-        //     {
-        //         // Token geçersiz, Unauthorized döndür
-        //         context.Result = new UnauthorizedResult();
-        //     }
-        // }
-        // else
-        // {
-        //     // Token bulunamadı
-        //     context.Result = new UnauthorizedResult();
-        // }
-        // if (response.IsLogin == false)
-        // {
-        //     return;//token yok
-        // }
-
-        // if (context?.HttpContext?.User?.Identity?.IsAuthenticated == true)
-        // {
-        //     Log.Information("OnActionExecuted Headers IsAuthenticated");
-        //     Log.Information("OnAuthorization Headers username" + context?.HttpContext?.User?.Claims?.FirstOrDefault(c => c.Type == "username").Value?.ToString());
-        //     Log.Information("OnAuthorization Headers FirstOrDefault" + context?.HttpContext?.User?.Claims?.FirstOrDefault().Value?.ToString());
-        //     Log.Information("OnAuthorization Headers LastOrDefault" + context?.HttpContext?.User?.Claims?.LastOrDefault().Value?.ToString());
-        // }
-    }
-
-    private ClaimsPrincipal ValidateToken(string token)
-    {
-        var tokenHandler = new JwtSecurityTokenHandler();
-
-        // Token doğrulama ayarları
-        var validationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true, // Token'ı hangi uygulamanın yayınladığını kontrol et
-            ValidateAudience = true, // Token'ın hangi uygulamaya ait olduğunu kontrol et
-            ValidateLifetime = true, // Token'ın süresinin geçerliliğini kontrol et
-            ValidateIssuerSigningKey = true, // Token'ın doğru imzalandığını kontrol et
-            ValidIssuer = "your-issuer", // Token'ı yayınlayan uygulama
-            ValidAudience = "your-audience", // Token'ın kullanılacağı uygulama
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your-secure-key"))
-        };
-
-        SecurityToken validatedToken;
-        var principal = tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
-        return principal;
+        Log.Information("{LogPrefix} - Authorization process ended", logPrefix);
     }
 }
-// public class CustomAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
-// {
-//     private readonly bool _useBaseAuthorizeLogic;
-//     private readonly string _requiredClaim;
-
-//     public CustomAuthorizeAttribute(string authenticationSchemes, string requiredClaim = null, bool useBaseAuthorizeLogic = true)
-//     {
-//         AuthenticationSchemes = authenticationSchemes;
-//         _requiredClaim = requiredClaim;
-//         _useBaseAuthorizeLogic = useBaseAuthorizeLogic;
-//     }
-
-//     public void OnAuthorization(AuthorizationFilterContext context)
-//     {
-//         // Eğer base authorize logic kullanılacaksa önce AuthorizeAttribute çalıştırılır
-//         if (_useBaseAuthorizeLogic)
-//         {
-//             var defaultAuthorize = new AuthorizeAttribute
-//             {
-//                 AuthenticationSchemes = AuthenticationSchemes
-//             };
-
-//             var defaultAuthorizationFilter = new AuthorizationFilter(context.HttpContext.RequestServices, defaultAuthorize);
-//             defaultAuthorizationFilter.OnAuthorization(context);
-
-//             // Eğer mevcut yetkilendirme başarısızsa (Unauthorized/Forbidden), devam etmiyoruz
-//             if (context.Result != null)
-//                 return;
-//         }
-
-//         // Ek Claim kontrolü yapılır
-//         if (!string.IsNullOrEmpty(_requiredClaim))
-//         {
-//             var user = context.HttpContext.User;
-
-//             if (!user.Identity?.IsAuthenticated ?? true ||
-//                 !user.Claims.Any(c => c.Type == _requiredClaim))
-//             {
-//                 context.Result = new ForbidResult();
-//                 return;
-//             }
-//         }
-
-//         // Ek duruma göre yetkilendirme yapılabilir
-//         if (SomeAdditionalLogic(context))
-//         {
-//             context.Result = new ForbidResult();
-//             return;
-//         }
-//     }
-
-//     private bool SomeAdditionalLogic(AuthorizationFilterContext context)
-//     {
-//         // Özel iş mantığınıza göre burada bir kontrol yapabilirsiniz
-//         return false; // Varsayılan olarak her zaman izin ver
-//     }
-// }
