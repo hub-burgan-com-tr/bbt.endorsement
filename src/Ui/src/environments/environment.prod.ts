45e5a46e91ae54c5b6a4a18ab882f73e9ae7fc06
Fatih <my.fatih.92@hotmail.com>
2024-12-26 22:15:14 +0300

    UI Url clear

--- src/Ui/src/environments/environment.prod.ts
+++ src/Ui/src/environments/environment.prod.ts
@@ -2,9 +2,5 @@ export const environment = {
   production: false,
   baseUrl: 'ENDORSEMENTAPI',
   loginUrl: 'LOGINURL',
-  clientSecret: 'CLIENTSECRET',
-  redirectUri: 'REDIRECTURI',
-  identityServerUrl: 'IDENTITYSERVERURL',
-  clientId: 'CLIENTID',
-  grantType: 'GRANTTYPE'
+  redirectUri: 'REDIRECTURI'
 };
